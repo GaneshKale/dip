5c0a09d6fa27aff6d763b8b1af1de19c203c5d27
Dipali <admin@localhost.localdomain>
2017-07-11 20:24:45 +0530

    First comment

---
+++ add.java
@@ -0,0 +1,17 @@
+class Add
+{
+	int addNumber(int number1,int number2)
+{
+	return number1+number2;
+
+}
+ public static void main(String args[])
+{
+	Add add=new Add();
+	int addition=addNumber(10,20);
+
+	System.out.println("Addition of The Numbers: "+addNumber);
+}
+
+
+
